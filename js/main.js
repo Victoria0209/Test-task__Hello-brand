ba0cc81732d51a1b81ed6aaeae2dc1680804d384
Victoria0209 <mineburn22@gmail.com>
2023-02-01 13:27:35 +0300

    add animation

--- js/main.js
+++ js/main.js
@@ -44,3 +44,15 @@ function closeOnClick() {
   hamb.classList.remove("active");
   body.classList.remove("noscroll");
 }
+
+window.counter = function() {
+    // this refers to the html element with the data-scroll-showCallback tag
+    var span = this.querySelector('span');
+    var current = parseInt(span.textContent);
+    
+    span.textContent = current + 1;
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+  ScrollTrigger.init();
+});

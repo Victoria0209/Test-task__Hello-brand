5750912239a9b9de6a4747cb275c02120b9c8123
Victoria0209 <mineburn22@gmail.com>
2023-02-02 21:27:17 +0300

    correction fucn for pop-up

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,23 +1,73 @@
-let popupReserve = document.querySelector('.b-popup')
-let btnReserve = document.querySelector('.about-reserve__btn-reserve')
-let form = document.querySelector('.b-popup__form')
+const popupReserve = document.querySelector('.b-popup')
+const errorPopup = document.querySelector('.error-popup')
+const btnReserve = document.querySelector('.about-reserve__btn-reserve')
+const btnClosePopup = document.querySelector('.b-popup__btn-close')
+const btnError = document.querySelector('.error-popup__btn')
+const form = document.querySelector('.b-popup__form')
+const guestName = document.querySelector('.b-popup__name')
+const guestEmail = document.querySelector('.b-popup__email')
 
 function openPopup() {
-    popupReserve.classList.add('b-popup-active')
-    popupReserve.classList.remove('b-popup-close')
+    popupReserve.classList.add('b-popup-active');
+    popupReserve.classList.remove('b-popup-close');
+}
+btnReserve.addEventListener('click', (event) => {
+    event.preventDefault();
+    openPopup();
+})
+
+
+function closePopup() {
+    popupReserve.classList.remove('b-popup-active');
+    popupReserve.classList.add('b-popup-close');
 }
 
-function closePopup(evt) {
+function closePopupEsc(evt) {
     if (evt.key === 'Escape') {
         evt.preventDefault();
-        popupReserve.classList.remove('b-popup-active')
-        popupReserve.classList.add('b-popup-close')
+        closePopup();
     }
 }
 
-btnReserve.addEventListener('click', (event) => {
-    event.preventDefault()
-    openPopup()
+btnClosePopup.addEventListener('click', (event) => {
+    event.preventDefault();
+    closePopup();
 })
-document.addEventListener('keydown', closePopup);
 
+document.addEventListener('keydown', closePopupEsc);
+
+
+function showMsgError() {
+    errorPopup.classList.add('error-popup-active');
+    errorPopup.addEventListener('click', () => {
+        errorPopup.classList.remove('error-popup-active');
+    })
+}
+
+async function createReserveTable(name, email) {
+    try {
+        const response = await fetch('https://api.jsonserver.io/data', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Jsio-Token': 'b36f694c4458eabf9746671dd64c7407'
+            },
+            body: JSON.stringify({
+                "name": name,
+                "email": email
+            })
+        });
+        const data = response.json();
+    } catch (e) {
+        showMsgError();
+    } finally {
+        closePopup();
+    }
+
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createReserveTable(guestName.value, guestEmail.value);
+})
